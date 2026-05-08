ae5cde2698947a2d439933159242621b1d749a93
MacBook Air M4 <macbook@MacBooks-MacBook-Air.local>
2026-05-08 16:41:11 -0400

    Initial firmware skeleton for RP2350 (Phase 1 preparation)

---
+++ firmware/master-hat-firmware/src/main.cpp
@@ -0,0 +1,11 @@
+#include <Arduino.h>
+
+void setup() {
+    Serial.begin(115200);
+    // TODO: Initialize Bus Sniffing PIO
+    // TODO: Initialize I2S DAC
+}
+
+void loop() {
+    // Phase 1: Orchestra-90 logic will happen in PIO/Interrupts
+}
